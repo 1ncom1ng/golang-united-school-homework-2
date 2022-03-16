dc69b2186e33e7fcd7997551807b5acc0d857a8c
1ncom1ng <anatoliy.metkechekov@gmail.com>
2022-03-16 18:36:30 +0300

    Update solution.go

--- solution.go
+++ solution.go
@@ -12,17 +12,17 @@ import (
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum int) float64 {
+func CalcSquare(sideLen float64, sideNum int) float64 {
 	var square float64
 	const SidesTriangle = 3
 	const SidesSquare = 4
 	const SidesCircle = 0
 
-	if sidesNum == 0 {
+	if sideNum == 0 {
 		square = math.Pi * sideLen * sideLen
-	} else if sidesNum == 3 {
+	} else if sideNum == 3 {
 		square = math.Sqrt(3) * sideLen * sideLen / 4
-	} else if sidesNum == 4 {
+	} else if sideNum == 4 {
 		square = sideLen * sideLen
 	} else {
 		square = 0
